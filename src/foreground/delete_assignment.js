981fe0f58e4b6f3c057ba4a39beb59e9b056633a
Jack DeVries <jdevries3133@gmail.com>
2021-02-19 00:00:00 +0000

    feat: DONE! (again (but for real this time))

--- src/foreground/delete_assignment.js
+++ src/foreground/delete_assignment.js
@@ -51,7 +51,6 @@ const followThroughDelete = async () => {
   try {
     deleteOption = getOne("/html/body/div[11]/div/div/span[2]");
   } catch (e) {
-    console.warn("deleteOption selection failed; retrying with div12");
     deleteOption = getOne("/html/body/div[12]/div/div/span[2]");
   }
   naturalClick(deleteOption);
@@ -62,7 +61,6 @@ const followThroughDelete = async () => {
       '//*[@id="yDmH0d"]/div[11]/div/div[2]/div[3]/div[2]'
     );
   } catch (e) {
-    console.warn("confirmDelete selection failed; retrying with div12");
     confirmDelete = getOne(
       '//*[@id="yDmH0d"]/div[12]/div/div[2]/div[3]/div[2]'
     );
@@ -128,11 +126,19 @@ const deleteTopic = async (topicName) => {
   let node;
   node = selectTopic(topicName);
   const commonRootNode = node.parentElement.parentElement.parentElement;
-  const numTopics = lenTopic(topicName);
-  for (let i = 0; i < numTopics; i++) {
-    console.log(`i = ${i}`);
-    await deleteFirstAssignment(commonRootNode);
-    await sleep(2000);
+  for (let j = 0; j < 2; j++) {
+    /**
+     * doing this twice makes it a bit more resistant to hangups. If the site
+     * lags and causes topic deletion to miss, most topics will still be
+     * deleted. This creates the equivalent to checking and deleting any
+     * remaining assignments if they exist one more time before deleting the
+     * topic too.
+     */
+    const numTopics = lenTopic(topicName);
+    for (let i = 0; i < numTopics; i++) {
+      await deleteFirstAssignment(commonRootNode);
+      await sleep(2000);
+    }
   }
   const topicMenu = nodeToXpath(
     commonRootNode,
@@ -142,6 +148,7 @@ const deleteTopic = async (topicName) => {
   naturalClick(topicMenu);
   await sleep(1000);
   await followThroughDelete();
+  await sleep(2000);
 };
 
 export const deleteTopics = async (topicNames) => {
@@ -150,14 +157,13 @@ export const deleteTopics = async (topicNames) => {
    * @returns {array} List of topics that failed to be deleted.
    */
   let failed = [];
-  topicNames.forEach(async (name, i) => {
-    // TODO: these functions are all getting run at the same time.
-    // I think a traditional for loop will fix
+  for (let i = 0; i < topicNames.length; i++) {
     try {
-      await deleteTopic(name);
+      await deleteTopic(topicNames[i]);
     } catch (e) {
-      console.warn(`Topic "${name}" failed due to ${e}`);
-      failed.push(name);
+      console.warn(`Topic "${topicNames[i]}" failed due to ${e}`);
+      failed.push(topicNames[i]);
     }
-  });
+  }
+  console.info("All topis deleted.");
 };
